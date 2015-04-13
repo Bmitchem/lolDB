01d95f9f9b6ca67438f80f3e3bb3db819eb52965
bob <bmitchem@morristechnology.com>
2015-04-12 23:52:20 -0400

    game import more random now

--- armory/management/commands/import_summoners.py
+++ armory/management/commands/import_summoners.py
@@ -4,14 +4,19 @@ from riot_api import RiotInterface
 from armory import models
 import sys
 from time import sleep
+import random
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+
         summoner_list = models.Summoners.objects.all().order_by('-dateAdded')
+        query_set = []
+        for i in range(30):
+            query_set.append(summoner_list(random.randint(0,len(summoner_list))))
 
         ri = RiotInterface()
         counter = 0
-        for index, summoner in enumerate(summoner_list):
+        for index, summoner in enumerate(query_set):
             counter += 1
             if counter < 30:
                 print "Importing history for " + str(summoner.summonerId)
