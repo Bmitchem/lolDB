96b725b0bbfcb010f55d259733342440211f4feb
bob <bmitchem@morristechnology.com>
2015-04-12 23:39:55 -0400

    added game import shell script for cron

--- armory/management/commands/import_summoners.py
+++ armory/management/commands/import_summoners.py
@@ -2,6 +2,7 @@ __author__ = 'bob'
 from django.core.management.base import BaseCommand, CommandError
 from riot_api import RiotInterface
 from armory import models
+import sys
 from time import sleep
 
 class Command(BaseCommand):
@@ -9,8 +10,11 @@ class Command(BaseCommand):
         summoner_list = models.Summoners.objects.all().order_by('-dateAdded')
 
         ri = RiotInterface()
+        counter = 0
         for index, summoner in enumerate(summoner_list):
-            print "Importing history for " + str(summoner.summonerId)
-            ri.import_summoner_id_from_pull(summoner.summonerId)
-
+            if counter < 10:
+                print "Importing history for " + str(summoner.summonerId)
+                ri.import_summoner_id_from_pull(summoner.summonerId)
+            else:
+                sys.exit()
         # ri.import_summoner_id_from_pull(25500646)
