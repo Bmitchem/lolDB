8a3d0c9a49bcf12922d210fd0987dcb00fe46663
bob <bmitchem@morristechnology.com>
2015-04-15 23:16:59 -0400

    i always forget to commit migrations

---
+++ armory/migrations/0005_auto_20150416_0258.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('armory', '0004_participantstats_summonerid'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='game',
+            name='fellowPlayers',
+            field=models.ManyToManyField(to='armory.Participant', db_index=True),
+            preserve_default=True,
+        ),
+    ]
